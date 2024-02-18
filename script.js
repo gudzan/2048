1be7130ae1007ce8987d4986526d6886b3450866
gudzan <andreeva.anva@gmail.com>
2024-02-18 12:32:50 +0300

    commit

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ class Cell {
     this.locationX = locationX.toString();
     this.locationY = locationY.toString();
   }
+  getNumber() {
+    return this.number;
+  }
   setNumber(number) {
     this.number = number;
   }
@@ -11,52 +14,136 @@ class Cell {
     return this.locationX.toString() + this.locationY.toString();
   }
 }
+
 let cells = [];
-start();
 
-function start() {
-  createTable();
-  let cell1 = cells[getRandomStartNumber()].setNumber(getRandomNumber());
-  let cell2 = cells[getRandomStartNumber()].setNumber(getRandomNumber());
+window.addEventListener("load", () => {
+  createStartCells();
+  addNumberToCells();
   console.log(cells);
-  addNumberToCells()
-}
+});
 
-function getRandomStartNumber() {
-  // случайное число от min до (max+1)
-  let rand = Math.random() * 16;
-  return Math.floor(rand);
-}
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+  switch (key) {
+    case "ArrowUp":
+      moveToUp();
+      break;
+    case "ArrowDown":
+      moveToDown();
+      break;
+    case "ArrowLeft":
+      console.log('Нажата клавиша "Влево"!');
+      break;
+    case "ArrowRight":
+      console.log('Нажата клавиша "Вправо"!');
+      break;
+  }
+});
 
-function addNumberToCells(){
-  cells.forEach(cell => {
-    if(cell.number!==0){
-      let cellElement = document.getElementById(cell.getLocation());
-      cellElement.classList.add("cell_number_" + cell.number);
-      cellElement.classList.add("cell_number");
+function moveToDown() {
+  if (canMove()) {
+    for (let y = 0; y < 4; y++) {
+      columnMoveDown(y);
     }
+  }
+  console.log(cells);
+  addNumberToCells();
+}
+
+function moveToUp() {
+  if (canMove()) {
+    for (let y = 0; y < 4; y++) {
+      columnMoveUp(y);
+    }
+  }
+  console.log(cells);
+  addNumberToCells();
+}
+
+function canMove() {
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 4; x++) {
+      if (cells[x][y].getNumber() === 0) {
+        console.log(true);
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function addNumberToCells() {
+  cells.forEach((row) => {
+    row.forEach((cell) => {
+      let cellElement = document.getElementById(cell.getLocation());
+      cellElement.removeAttribute("class");
+      if (cell.number === 0) {
+        cellElement.innerHTML = ``;
+      } else {
+        cellElement.classList.add("cell_number");
+        cellElement.classList.add("cell_number_" + cell.number);
+        cellElement.innerHTML = `${cell.number}`;
+      }
+    });
   });
 }
 
-function getRandomNumber() {
-  return Math.round(Math.random()) === 0 ? 2 : 4;
-}
-
-function getRandomLocation() {
-  return Math.floor(Math.random() * 4);
-}
-
-function createTable() {
+function columnMoveUp(y) {
+  let numbers = getColumnWithoutZero(y)
   for (let x = 0; x < 4; x++) {
-    for (let y = 0; y < 4; y++) {
-      const cell = new Cell(0, x, y);
-      cells.push(cell);
+    if (x < numbers.length) {
+      cells[x][y].setNumber(numbers[x]);
+    }
+    else {
+      cells[x][y].setNumber(0);
     }
   }
 }
 
-function toHTML(cell) {
-  return `
-    <td id = "${cell.getLocation()}"></td>">${user.name}</li>
-  `;
+function getColumnWithoutZero(y){
+  let numbers = [];
+  for (let x = 0; x < 4; x++) {
+    if (cells[x][y].number !== 0) {
+      numbers.push(cells[x][y].number);
+    }
+  }
+  return numbers
+}
+
+function columnMoveDown(y) {
+  let numbers = getColumnWithoutZero(y).toReversed()
+  for (let x = 3, i=0; x >= 0; x--,i++) {
+    if (i < numbers.length) {
+      cells[x][y].setNumber(numbers[i]);
+    }
+    else {
+      cells[x][y].setNumber(0);
+    }
+  }
+}
+
+function createStartCells() {
+  for (let x = 0; x < 4; x++) {
+    let rowCell = [];
+    for (let y = 0; y < 4; y++) {
+      const cell = new Cell(getRandomStartLocation(), x, y);
+      rowCell.push(cell);
+    }
+    cells.push(rowCell);
+  }
+  cells[getRandomStartLocation()][getRandomStartLocation()].setNumber(
+    getRandomStartNumber()
+  );
+  cells[getRandomStartLocation()][getRandomStartLocation()].setNumber(
+    getRandomStartNumber()
+  );
+}
+
+function getRandomStartLocation() {
+  return Math.floor(Math.random() * 4);
+}
+
+function getRandomStartNumber() {
+  return Math.round(Math.random()) === 0 ? 2 : 4;
 }
